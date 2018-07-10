ba99f9508c41896e07a6d063748877dd604ad82f
bruceMJ <mjdev@live.in>
2018-07-10 17:01:39 +0530

    scrollmagic animation initiated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+declare var ease, TimelineMax,TweenMax,Power4,Power1,Power2,Power3,Bounce,Cubic,Back, Elastic:any;
+import "gsap";
+import * as ScrollMagic from 'scrollmagic';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  ctrl = new ScrollMagic.Controller();
+
+  doTwinAnimation() {
+    let tween = TweenMax.staggerFromTo(".animate4", 2, {left: 700}, {left: 0, ease: Back.easeOut}, 0.15);
+
+    new ScrollMagic.Scene({
+      triggerElement: '#trigger4',
+      duration: 300
+    })
+      .setTween(tween)
+      .addIndicators({name: "staggering"})
+      .addTo(this.ctrl);
+  }
 }
